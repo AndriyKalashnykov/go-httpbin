c135672bff976760dca99706440a29d46dc76e33
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2023-10-15 22:38:46 -0400

    Cut v0.0.36 release
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

---
+++ scripts/install-cross-libs-ubuntu.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+sudo apt install software-properties-common -y
+sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+
+sudo apt-get install -y cmake git patch gettext unzip musl-tools lzma-dev libssl-dev libxml2-dev libghc-zlib-dev libexpat1-dev bzip2 cpio
+
+sudo apt-get install -y build-essential gcc libmpfr-dev libgmp3-dev libmpc-dev gcc-aarch64-linux-gnu --install-suggests
+sudo apt-get install -y gcc-arm-linux-gnueabihf libc6-dev-armhf-cross gcc-aarch64-linux-gnu libc6-dev-arm64-cross --no-install-recommends
+
+## windows
+sudo apt-get install -y *-w64-x86-*
